1dc948ae87c0a54bf31973a2b153d21e07ec7dd1
Shaurya gupta <shauryagupta@Shauryas-Laptop.local>
2025-01-21 21:25:17 -0800

    Trying -2

--- app/(tabs)/signIn.tsx
+++ app/(tabs)/signIn.tsx
@@ -1,18 +1,90 @@
 import * as WebBrowser from "expo-web-browser";
 import React, { useState, useEffect } from 'react';
-import { signInExperience as UserSignIn, setUserInfo } from '@/components/UserSignIn';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StyleSheet } from 'react-native';
+import * as Google from "expo-auth-session/providers/google";
+import { AuthSessionResult } from 'expo-auth-session';
+import { Text, Button, TextInput, View } from 'react-native';
+import BuddyButton from '@/components/BuddyButton';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 WebBrowser.maybeCompleteAuthSession();
 
+//client IDs from .env
+const config = {
+    androidClientId: process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID,
+    iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID,
+    webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
+};
+
+console.log(`WEB_CLIENT_ID: ${config.webClientId}`);
+
+// const getUserInfo = async (token: string | undefined) => {
+//     //absent token
+//     if (!token) return;
+//     //present token
+//     let response: Response | null = null;
+//     try {
+//         response = await fetch(
+//             "https://www.googleapis.com/userinfo/v2/me",
+//             {
+//                 headers: { Authorization: `Bearer ${token}` },
+//             }
+//         );
+//         const user = await response.json();
+//         //store user information  in Asyncstorage
+//         await AsyncStorage.setItem("user", JSON.stringify(user));
+//         setUserInfo(user);
+//     } catch (error) {
+//         console.error(
+//             "Failed to fetch user data:",
+//             response?.status,
+//             response?.statusText
+//         );
+//     }
+// };
+
+// const signInWithGoogle = async (response: AuthSessionResult | null) => {
+//     try {
+//         const userJSON = await AsyncStorage.getItem("user");
+
+//         if (userJSON) {
+//             // If user information is found in AsyncStorage, parse it and set it in the state
+//             setUserInfo(JSON.parse(userJSON));
+//         } else if (response?.type === "success") {
+//             // If no user information is found and the response type is "success" (assuming response is defined),
+//             // call getUserInfo with the access token from the response
+//             await getUserInfo(response?.authentication?.accessToken);
+//         }
+//     } catch (error) {
+//         // Handle any errors that occur during AsyncStorage retrieval or other operations
+//         console.error("Error retrieving user data from AsyncStorage:", error);
+//     }
+// };
+
 export default function SignIn() {
+    const [userInfo, setUserInfo] = useState(null);
+    const [request, response, promptAsync] = Google.useAuthRequest(config);
+
+    // useEffect(() => {
+    //     signInWithGoogle(response);
+    // }, [response]);
+
+    //log the userInfo to see user details
+    console.log(JSON.stringify(userInfo))
+
     return (
         <GestureHandlerRootView style={styles.container}>
-            <UserSignIn />
+            <View style={{ padding: 10 }}>
+                {/* <BuddyButton theme="buddy" label="sign in with google" inputType='' onPress={submitUserInputText} /> */}
+                <Text>{JSON.stringify(userInfo, null, 2)}</Text>
+                <Button title="sign in with google" onPress={() => { promptAsync() }} />
+            </View>
         </GestureHandlerRootView>
     );
 }
+
 
 const styles = StyleSheet.create({
     container: {
